e026047900b6f9e1027c3c696d05faa71ed64b80
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-07-26 22:53:16 +0500

    outsourcing action types

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/action';
 
 class Counter extends Component {
     state = {
@@ -55,10 +56,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD', val: 5}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 5}),
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 5}),
+        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 5}),
         onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
         onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
     };
