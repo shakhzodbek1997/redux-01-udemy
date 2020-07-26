b92889c52b36c37435a6dbdfd71746fc1ea5b1f2
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-07-26 14:25:37 +0500

    restart

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,24 +2,13 @@ const initialState = {
     counter: 0
 }
 
-const reducer = (state = initialState, action) =>{
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
     return state;
+};
+
 export default reducer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
