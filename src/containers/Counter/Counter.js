9f03c8537c814b6c68f92ee2c94472781f9e5176
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-07-26 17:27:40 +0500

    Updating State Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -34,6 +34,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -41,7 +48,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -49,8 +57,10 @@ const mapDispatchToProps = dispatch => {
     return {
         onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD', val: 3}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 33})
+        onAddCounter: () => dispatch({type: 'ADD', val: 5}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,27 +1,41 @@
 const initialState = {
-    counter: 0
-}
+    counter: 0,
+    results: []
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({},state);
+            newState.counter = state.counter + 1
+            return newState;
+
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
+
         case 'ADD':
             return {
-                counter: state.counter + action.val  //3
+                ...state,
+                counter: state.counter + action.val  //5
             }
+
         case 'SUBTRACT':
             return {
-                counter: state.counter - action.val  //33
+                ...state,
+                counter: state.counter - action.val  //5
+            }
+
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+
             }
     }
-
     return state;
 };
 
