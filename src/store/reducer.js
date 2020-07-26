8d862674b606aff2fee34515c2f041a2fe022be8
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-07-26 16:43:06 +0500

    Passing and Retrieving Data with Action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,23 @@ const reducer = (state = initialState, action) => {
             counter: state.counter + 1
         }
     }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + action.val  //3
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.val  //33
+        }
+    }
+
+
     return state;
 };
 
